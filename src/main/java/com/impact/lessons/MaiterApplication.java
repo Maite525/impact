50c09d8cb553ae2d101c376ce8304bc27e6eb7ef
maiter <matveimocanu05@gmail.com>
2026-01-27 18:35:40 +0200

    Initial commit

---
+++ src/main/java/com/impact/lessons/MaiterApplication.java
@@ -0,0 +1,13 @@
+package com.impact.lessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiterApplication.class, args);
+	}
+
+}
